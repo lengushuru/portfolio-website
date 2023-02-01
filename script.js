1c09c3517001520480c770af35c5b5c834d7dc2b
lengushuru charles <charleslengushuru@gmail.com>
2023-02-01 15:44:59 +0300

    Update script.js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// code for the mobile menu
+
 const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('nav-toggle');
 const closeMenu = document.getElementById('nav-close');
@@ -12,102 +14,151 @@ const navLinks = document.querySelectorAll('.nav-item');
 
 navLinks.forEach((n) => n.addEventListener('click', () => navMenu.classList.remove('show')));
 
-//code for work section
-//Array of objects for the projects in the portfolio
+// code for work section
+// Array of objects for the projects in the portfolio
 
 const Portfolio = [
   {
-  id: 'first',
-  modal_class: 'modal_class1',
-  close_popup:'close-popup1',
-  title:'Multi-Post Stories',
-  description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-  technologies:['html','bootstrap','Ruby on Rails'],
-  seeLive:'#',
-  seePeoject:'#',
+    id: 'zero',
+    modal_class: 'modal_class0',
+    close_popup: 'close-popup0',
+    title: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['html', 'bootstrap', 'Ruby on Rails'],
+    seeLive: '#',
+    seePeoject: '#',
   },
   {
-  id: 'second',
-  modal_class: 'modal_class2',
-  close_popup:'close-popup2',
-  title:'Data Dashboard Healthcare',
-  description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-  technologies:['html','bootstrap','Ruby'],
-  seeLive:'#',
-  seePeoject:'#',
+    id: 'first',
+    modal_class: 'modal_class1',
+    close_popup: 'close-popup1',
+    title: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['html', 'bootstrap', 'Ruby on Rails'],
+    seeLive: '#',
+    seePeoject: '#',
   },
   {
-  id: 'third',
-  modal_class: 'modal_class3',
-  close_popup:'close-popup3',
-  title:'Website Protfolio',
-  description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard some additional text will be added later',
-  technologies:['HTML','CSS','Javasript'],
-  seeLive:'#',
-  seePeoject:'#',
+    id: 'second',
+    modal_class: 'modal_class2',
+    close_popup: 'close-popup2',
+    title: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    seeLive: '#',
+    seePeoject: '#',
+  },
+  {
+    id: 'third',
+    modal_class: 'modal_class3',
+    close_popup: 'close-popup3',
+    title: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard some additional text will be added later',
+    technologies: ['HTML', 'CSS', 'Javasript'],
+    seeLive: '#',
+    seePeoject: '#',
   },
   {
     id: 'fourth',
-    close_popup:'close-popup4',
+    close_popup: 'close-popup4',
     modal_class: 'modal_class4',
-    title:'Profesional Art Printing Data More',
-    description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    technologies:['HTML','CSS','Javasript'],
-    seeLive:'#',
-    seePeoject:'#',
-    },
+    title: 'Profesional Art Printing Data More',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['HTML', 'CSS', 'Javasript'],
+    seeLive: '#',
+    seePeoject: '#',
+  },
   {
     id: 'fifth',
-    close_popup:'close-popup5',
+    close_popup: 'close-popup5',
     modal_class: 'modal_class5',
-    title:'Data Dashboard Healthcare',
-    description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    technologies:['HTML','CSS','Javasript'],
-    seeLive:'#',
-    seePeoject:'#',
+    title: 'Data Dashboard Healthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['HTML', 'CSS', 'Javasript'],
+    seeLive: '#',
+    seePeoject: '#',
   },
   {
     id: 'sixth',
-    close_popup:'close-popup6',
+    close_popup: 'close-popup6',
     modal_class: 'modal_class6',
-    title:'Website Protfolio',
-    description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
-    technologies:['HTML','CSS','Javasript'],
-    seeLive:'#',
-    seePeoject:'#',
-},
+    title: 'Website Protfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard',
+    technologies: ['HTML', 'CSS', 'Javasript'],
+    seeLive: '#',
+    seePeoject: '#',
+  },
 ];
 
+const topCard = document.createElement('div');
+topCard.innerHTML = `
+<div class="work-title">
+<h2>My Resent work</h2>
+<div></div>
+</div>
+<div class="container">
+<div class="work1">
 
-const cards = document.querySelector('.card')
+</div>
 
-//create a card for each project in the portfolio array and append it to the cards div in the html file 
+<div class="card"></div>
+
+</div>
+`;
+document.querySelector('.portfolio').appendChild(topCard);
+const cards = document.querySelector('.card');
+
+// create a card for each project in the portfolio array
+// and append it to the cards div in the html file
 Portfolio.forEach((project) => {
-  //map through the languages array and create a list item for each language and join them together to form a string
-  const stack = project.technologies.map((lang) => `<li>${lang}</li>`).join(''); 
-  console.log(stack)
+  // map through the languages array and create a list
+  // item for each language and join them together to form a string
+  const stack = project.technologies.map((lang) => `<li>${lang}</li>`).join('');
+  if (project.id === 'zero') {
+    const background = document.createElement('div');
+    background.className = 'background';
+    document.querySelector('.work1').appendChild(background);
 
-  const card = document.createElement('div');
-  card.classList.add('other-works');
-  card.innerHTML = `
+    const headCard = document.createElement('div');
+    headCard.className = 'work1-text';
+    headCard.innerHTML = `
+
+  <h3 class="work-subtitle">Multi-Post Stories</h3>
+    <p class="works-desk">A daily selection of privately personalized reads; no accounts or sign-ups required. has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy
+      text.</p>
+      <ul class="lang-list">
+      <li>css</li>
+      <li>html</li>
+      <li>bootstrap</li>
+      <li>Ruby</li>
+    </ul>
+    <a href="#" id = ${`${project.id}1`}>see project</a>
+  `;
+    document.querySelector('.work1').appendChild(headCard);
+  } else {
+    const card = document.createElement('div');
+    card.classList.add('other-works');
+    card.innerHTML = `
             <h3 class="card-subtitle2">${project.title}</h3>
             <p class="card-text2">${project.description}</p>
             <ul class="lang-list2">
             ${stack}
            </ul>
-           <div class="prj-link" id = ${project.id + '1'}><a class=''>See project</a></div>`
-  cards.appendChild(card);
-  document.querySelector('.card').appendChild(card);
+           <div class="prj-link" id = ${`${project.id}1`}><a class=''>See project</a></div>`;
+    cards.appendChild(card);
+    document.querySelector('.card').appendChild(card);
+  }
 });
 
-//modal window
+// modal window
 
 Portfolio.forEach((project) => {
-  //map through the languages array and create a list item for each language and join them together to form a string
-  const stack = project.technologies.map((lang) => `<li>${lang}</li>`).join(''); 
-  console.log(stack)
-  const modal = document.createElement('div')
-  cards.appendChild(modal)
+  // map through the languages array and create
+  // a list item for each language and join them together to form a string
+  const stack = project.technologies.map((lang) => `<li>${lang}</li>`).join('');
+  const modal = document.createElement('div');
+  cards.appendChild(modal);
   const popup = document.createElement('div');
   popup.id = project.id;
   popup.innerHTML = `
@@ -127,38 +178,23 @@ Portfolio.forEach((project) => {
           <div class="modal-btns">
             <div class="prj-btn">
               <a href= ${project.seeLive}>See Live</a>
+              <img src="images/see-live.png" alt="">
             </div>
             <div class="prj-btn">
               <a  href="./">See Source</a>
+              <img src="images/see-source.png" alt="">
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>`
+  </div>`;
   modal.appendChild(popup);
-  // document.querySelector('.card').appendChild(popup);
 });
 
-
-//code for popup window
-
-// const popup = document.getElementById('third');
-// const closePopup = document.getElementById('close-popup');
-// const openPopup = document.getElementById('third');
-
-// openPopup.addEventListener('click', () => {
-//   popup.style.display = 'flex';
-// }
-// );
-
-// closePopup.addEventListener('click', () => {
-//   popup.classList.remove('show');
-// }
-// );
-
-// code for showing popup window when clicking on the project card in the work section of the portfolio page 
-
+// code for showing popup window when clicking on the
+// project card in the work section of the portfolio page
+const openPopup0 = document.getElementById('zero1');
 const openPopup1 = document.getElementById('first1');
 const openPopup2 = document.getElementById('second1');
 const openPopup3 = document.getElementById('third1');
@@ -166,6 +202,7 @@ const openPopup4 = document.getElementById('fourth1');
 const openPopup5 = document.getElementById('fifth1');
 const openPopup6 = document.getElementById('sixth1');
 
+const popup0 = document.getElementById('zero');
 const popup1 = document.getElementById('first');
 const popup2 = document.getElementById('second');
 const popup3 = document.getElementById('third');
@@ -173,132 +210,82 @@ const popup4 = document.getElementById('fourth');
 const popup5 = document.getElementById('fifth');
 const popup6 = document.getElementById('sixth');
 
+openPopup0.addEventListener('click', () => {
+  popup0.style.display = 'flex';
+  document.querySelector('.modal_class0').style.display = 'flex';
+});
+
 openPopup1.addEventListener('click', () => {
   popup1.style.display = 'flex';
-    document.querySelector('.modal_class1').style.display = 'flex';
-}
-);
+  document.querySelector('.modal_class1').style.display = 'flex';
+});
 
 openPopup2.addEventListener('click', () => {
   popup2.style.display = 'flex';
   document.querySelector('.modal_class2').style.display = 'flex';
-}
-);
+});
 
 openPopup3.addEventListener('click', () => {
   popup3.style.display = 'flex';
   document.querySelector('.modal_class3').style.display = 'flex';
-}
-);
+});
 
 openPopup4.addEventListener('click', () => {
   popup4.style.display = 'flex';
   document.querySelector('.modal_class4').style.display = 'flex';
-}
-);
+});
 
 openPopup5.addEventListener('click', () => {
   popup5.style.display = 'flex';
   document.querySelector('.modal_class5').style.display = 'flex';
-}
-);
+});
 
 openPopup6.addEventListener('click', () => {
   popup6.style.display = 'flex';
   document.querySelector('.modal_class6').style.display = 'flex';
-}
-);
+});
 
-//code for closing popup window when clicking on the close button
+// code for closing popup window when clicking on the close button
 
 const closePopup1 = document.getElementById('close-popup1');
+const closePopup0 = document.getElementById('close-popup0');
 const closePopup2 = document.getElementById('close-popup2');
 const closePopup3 = document.getElementById('close-popup3');
 const closePopup4 = document.getElementById('close-popup4');
 const closePopup5 = document.getElementById('close-popup5');
 const closePopup6 = document.getElementById('close-popup6');
 
+closePopup0.addEventListener('click', () => {
+  popup0.style.display = 'none';
+  document.querySelector('.modal_class0').style.display = 'none';
+});
+
 closePopup1.addEventListener('click', () => {
   popup1.style.display = 'none';
   document.querySelector('.modal_class1').style.display = 'none';
-}
-);
+});
 
 closePopup2.addEventListener('click', () => {
   popup2.style.display = 'none';
   document.querySelector('.modal_class2').style.display = 'none';
-}
-);
+});
 
 closePopup3.addEventListener('click', () => {
   popup3.style.display = 'none';
   document.querySelector('.modal_class3').style.display = 'none';
-}
-);
+});
 
 closePopup4.addEventListener('click', () => {
   popup4.style.display = 'none';
   document.querySelector('.modal_class4').style.display = 'none';
-}
-);
+});
 
 closePopup5.addEventListener('click', () => {
   popup5.style.display = 'none';
   document.querySelector('.modal_class5').style.display = 'none';
-}
-);
+});
 
 closePopup6.addEventListener('click', () => {
   popup6.style.display = 'none';
   document.querySelector('.modal_class6').style.display = 'none';
-}
-);
-
-//code for closing popup window when clicking outside the popup window
-window.addEventListener('click', (e) => {
-  if (e.target === popup1) {
-    popup1.style.display = 'none';
-    // document.querySelector('.modal-content').style.display = 'none';
-  }
-}
-);
-
-window.addEventListener('click', (e) => {
-  if (e.target === popup2) {
-    popup2.style.display = 'none';
-    // document.querySelector('.modal-content').style.display = 'none';
-  }
-}
-);
-
-window.addEventListener('click', (e) => {
-  if (e.target === popup3) {
-    popup3.style.display = 'none';
-    // document.querySelector('.modal-content').style.display = 'none';
-  }
-}
-);
-
-window.addEventListener('click', (e) => {
-  if (e.target === popup4) {
-    popup4.style.display = 'none';
-    document.querySelector('.modal-content').style.display = 'none';
-  }
-}
-);
-
-window.addEventListener('click', (e) => {
-  if (e.target === popup5) {
-    popup5.style.display = 'none';
-    document.querySelector('.modal-content').style.display = 'none';
-  }
-}
-);
-
-window.addEventListener('click', (e) => {
-  if (e.target === popup6) {
-    popup6.style.display = 'none';
-    document.querySelector('modal-content').style.display = 'none';
-  }
-}
-);
+});
